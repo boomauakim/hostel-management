f3d4e1ef0fdd42d468aebc5ed531cbd3eba3dc48
Kamonwat <boomauakim@gmail.com>
2020-02-14 03:58:44 +0700

    Add available to hostels endpoint

--- backend/src/controllers/hostel.js
+++ backend/src/controllers/hostel.js
@@ -1,8 +1,10 @@
 /* eslint-disable no-underscore-dangle */
-const Joi = require('@hapi/joi');
+const Joi = require('@hapi/joi').extend(require('@hapi/joi-date'));
 const mongoose = require('mongoose');
+const moment = require('moment');
 
 const HostelModel = require('../models/hostel');
+const ReservationModel = require('../models/reservation');
 const body = require('../utils/body');
 const logger = require('../utils/logger');
 
@@ -172,6 +174,97 @@ const searchHotel = async (req, res) => {
   }
 };
 
+const getHostelAvailable = async (req, res) => {
+  const host = req.get('host');
+
+  try {
+    const schema = Joi.object({
+      before: Joi.string().length(24),
+      after: Joi.string().length(24),
+      limit: Joi.number().default(10),
+      start_at: Joi.date().format('YYYY-MM-DD').utc().required(),
+      end_at: Joi.date().format('YYYY-MM-DD').utc().required(),
+    });
+    const query = await schema.validateAsync(req.query);
+
+    try {
+      const hostelUnavailable = await ReservationModel.find(
+        {
+          date: {
+            $gte: query.start_at,
+            $lte: query.end_at,
+          },
+        },
+      )
+        .distinct('hostel');
+
+      let data = [];
+
+      if (query.before && query.after) {
+        return res.status(400).send(body.error(body.errorMsg.INVALID_PARAMETER, 'Must choose one of before or after.'));
+      }
+      if (query.before) {
+        data = await HostelModel
+          .find({
+            _id: {
+              $lt: new mongoose.Types.ObjectId(query.before),
+              $nin: hostelUnavailable,
+            },
+          })
+          .sort({ id: -1 })
+          .limit(parseInt(query.limit, 10));
+      } else if (query.after) {
+        data = await HostelModel
+          .find({
+            _id: {
+              $gt: new mongoose.Types.ObjectId(query.after),
+              $nin: hostelUnavailable,
+            },
+          })
+          .sort({ id: 1 })
+          .limit(parseInt(query.limit, 10));
+      } else {
+        data = await HostelModel
+          .find({ _id: { $nin: hostelUnavailable } })
+          .sort({ id: 1 })
+          .limit(parseInt(query.limit, 10));
+      }
+
+      const first = await HostelModel.findOne({ _id: { $nin: hostelUnavailable } }).sort({ id: 1 }).limit(1);
+      const last = await HostelModel.findOne({ _id: { $nin: hostelUnavailable } }).sort({ id: -1 }).limit(1);
+      let next = '';
+      let previous = '';
+
+      if (data.length > 0 && data[0] && !first._id.equals(data[0]._id)) {
+        previous = `${host}/api/hostels/available?before=${data[0]._id}&limit=${query.limit}&start_at=${moment(query.start_at).format('YYYY-MM-DD')}&end_at=${moment(query.end_at).format('YYYY-MM-DD')}`;
+      }
+      if (data.length > 0 && data[data.length - 1] && !last._id.equals(data[data.length - 1]._id)) {
+        next = `${host}/api/hostels/available?after=${data[data.length - 1]._id}&limit=${query.limit}&start_at=${moment(query.start_at).format('YYYY-MM-DD')}&end_at=${moment(query.end_at).format('YYYY-MM-DD')}`;
+      }
+
+      const resData = {
+        hostels: data,
+        paging: {
+          previous,
+          next,
+        },
+      };
+
+      return res.status(200).send(resData);
+    } catch (err) {
+      logger.error(err.stack);
+      return res.status(500).send(body.error());
+    }
+  } catch (err) {
+    if (err.details) {
+      const { message } = err.details[0];
+      return res.status(400).send(body.error(body.errorMsg.INVALID_PARAMETER, message));
+    }
+    logger.error(err.stack);
+    return res.status(500).send(body.error());
+  }
+};
+
 module.exports = {
-  getAllHostel, getHostel, createHostel, searchHotel,
+  getAllHostel, getHostel, getHostelAvailable, createHostel, searchHotel,
 };
--- backend/src/routes/hostel.js
+++ backend/src/routes/hostel.js
@@ -5,6 +5,7 @@ const hostel = require('../controllers/hostel');
 const router = express.Router();
 
 router.get('/', hostel.getAllHostel);
+router.get('/available', hostel.getHostelAvailable);
 router.get('/search', hostel.searchHotel);
 router.get('/:id', hostel.getHostel);
 router.post('/', hostel.createHostel);
