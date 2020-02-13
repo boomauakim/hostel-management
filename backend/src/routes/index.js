34c57799a4cc0f7ff5f8f37b3493f215eef6d4a0
Kamonwat <boomauakim@gmail.com>
2020-02-14 03:35:31 +0700

    Add me endpoint

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,11 +4,13 @@ const jwt = require('express-jwt');
 const auth = require('./auth');
 const booking = require('./booking');
 const hostel = require('./hostel');
+const me = require('./me');
 
 const router = express.Router();
 
 router.use('/', auth);
 router.use('/bookings', jwt({ secret: process.env.SECRET_KEY }), booking);
 router.use('/hostels', jwt({ secret: process.env.SECRET_KEY }), hostel);
+router.use('/me', jwt({ secret: process.env.SECRET_KEY }), me);
 
 module.exports = router;
